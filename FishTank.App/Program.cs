a504e8d206d2fdc82cc78839b02f4bfc210af652
Evie Skinner <evie.skinner@teamriver.com>
2020-03-28 16:31:24 +0000

    Removed and re-added FishTank.App

--- FishTank.App/Program.cs
+++ FishTank.App/Program.cs
@@ -6,7 +6,7 @@ namespace FishTank.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello fish!");
         }
     }
 }
